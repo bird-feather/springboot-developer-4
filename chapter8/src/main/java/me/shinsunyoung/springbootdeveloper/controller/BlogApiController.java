5f763839e57ffee7d3cf3a595a42c57c7265c8f2
s.sunyoung <ajufresh@gmail.com>
2026-01-27 21:40:13 +0900

    feat: 글쓰기 제안 API 추가

--- chapter8/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiController.java
+++ chapter8/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiController.java
@@ -2,10 +2,9 @@ package me.shinsunyoung.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.domain.Article;
-import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
-import me.shinsunyoung.springbootdeveloper.dto.ArticleResponse;
-import me.shinsunyoung.springbootdeveloper.dto.UpdateArticleRequest;
+import me.shinsunyoung.springbootdeveloper.dto.*;
 import me.shinsunyoung.springbootdeveloper.service.BlogService;
+import me.shinsunyoung.springbootdeveloper.service.WritingAssistantService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +14,10 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class BlogApiController {
+
     private final BlogService blogService;
+    private final WritingAssistantService writingAssistantService;
+
     @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
@@ -53,5 +55,13 @@ public class BlogApiController {
         Article updatedArticle = blogService.update(id, request);
         return ResponseEntity.ok()
                 .body(updatedArticle);
+    }
+
+    @PostMapping("/api/writing-assist")
+    public ResponseEntity<WritingSuggestionsResponse> writingAssist(@RequestBody WritingSuggestionRequest request) {
+        WritingSuggestionsResponse response = writingAssistantService.getWritingAssist(request);
+
+        return ResponseEntity.ok()
+                .body(response);
     }
 }
