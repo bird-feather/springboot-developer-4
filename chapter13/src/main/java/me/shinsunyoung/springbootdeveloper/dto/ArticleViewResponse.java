98b3a5580e7e8a5028140c23a3202b99fff6e92a
shinsunyoung <ajufresh@gmail.com>
2026-02-19 01:55:53 +0900

    fix: chapter13 예제 코드 수정

--- chapter13/src/main/java/me/shinsunyoung/springbootdeveloper/dto/ArticleViewResponse.java
+++ chapter13/src/main/java/me/shinsunyoung/springbootdeveloper/dto/ArticleViewResponse.java
@@ -12,14 +12,16 @@ public class ArticleViewResponse {
     private Long id;
     private String title;
     private String content;
-    private LocalDateTime createdAt;
     private String author;
+    private String imageUrl;
+    private LocalDateTime createdAt;
 
     public ArticleViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
-        this.createdAt = article.getCreatedAt();
         this.author = article.getAuthor();
+        this.imageUrl = article.getImageUrl();
+        this.createdAt = article.getCreatedAt();
     }
 }
